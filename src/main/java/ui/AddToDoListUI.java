ec8d69d33b097ffd6b1bf6319f39e759c704b147
Matteo <S296340@studenti.polito.it>
2024-06-05 21:49:59 +0200

    AddToDoListUIUI working, 232263

--- src/main/java/ui/AddToDoListUI.java
+++ src/main/java/ui/AddToDoListUI.java
@@ -1,13 +1,29 @@
 package ui;
 
 import controller.AddToDoListController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import model.GreenSpace;
 import model.Task;
 import ui.utils.Utils;
+
+import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class AddToDoListUI implements Runnable {
      private AddToDoListController controller;
@@ -17,17 +33,129 @@ public class AddToDoListUI implements Runnable {
     private String urgency;
     private String greenSpaceName;
 
+    @FXML
+    private Button btnConfirm;
+
+    @FXML
+    private ChoiceBox<GreenSpace> choiceGreenSpace;
+
+    @FXML
+    private Label lblDescription;
+    @FXML
+    private Label lblNameGreenSpace;
+
+    @FXML
+    private Label lblNameTask;
+
+    @FXML
+    private Label lblUrgency;
+
+    @FXML
+    private CheckBox optHigh;
+    @FXML
+    private TextField txtHours;
+    @FXML
+    private TextField txtDays;
+
+    @FXML
+    private CheckBox optLow;
+
+    @FXML
+    private CheckBox optMedium;
+
+    @FXML
+    private TextField txtDescription;
+
+    @FXML
+    private TextField txtDuration;
+    @FXML
+    private Button btnShowGreenSpaces;
+    @FXML
+    private TextField txtNameTask;
+
     public AddToDoListUI() {
         controller= new AddToDoListController();
     }
 
     public void run() {
-        System.out.println("\n\n--- Add a new entry in the to-do-list ------------------------");
-        requestTask();
-        requestEntryData();
-        requestGreenSpace();
-        submitData();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("AddToDoListUI.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+//           e.printStackTrace();
+            Alert a = new Alert(Alert.AlertType.ERROR, "'authentication' UI not open");
+            a.showAndWait();
+        }
+
     }
+    @FXML
+    void handleShowGreenSpace(ActionEvent event) {
+        List<GreenSpace> greenSpaces=controller.getGreenSpaceList();
+        ObservableList<GreenSpace> greenSpaces1 = FXCollections.observableArrayList();
+        greenSpaces1.addAll(greenSpaces);
+        this.choiceGreenSpace.setItems(greenSpaces1);
+    }
+    @FXML
+    void handleConfirm(ActionEvent event) {
+        boolean flag=true;
+        this.name=txtNameTask.getText();
+        if (this.name.equals("")) {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Please enter a task name");
+            a.showAndWait();
+            flag=false;
+        }
+        this.description=txtDescription.getText();
+        if (this.description.equals("")) {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Please enter a description");
+            a.showAndWait();
+            flag=false;
+        }
+        String days=txtDays.getText();
+        if (days.equals("")) {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Please enter a day");
+            a.showAndWait();
+            flag=false;
+        }
+        String hours=txtHours.getText();
+        if (hours.equals("")) {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Please enter a hour");
+            a.showAndWait();
+            flag=false;
+        }
+        String durationStr = "P" + days + "DT" + hours + "H" ;
+        try {
+            duration = Duration.parse(durationStr);
+        } catch (Exception e) {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Invalid duration");
+            a.showAndWait();
+            flag=false;
+        }
+        if ((optHigh.isSelected() && optLow.isSelected()) || (optMedium.isSelected() && optHigh.isSelected()) || (optMedium.isSelected() && optLow.isSelected())){
+            Alert a=new Alert(Alert.AlertType.ERROR, "Please enter just one level of urgency");
+            a.showAndWait();
+            flag=false;
+        }
+        else if (!optHigh.isSelected() && !optLow.isSelected() && !optMedium.isSelected()){
+            Alert a=new Alert(Alert.AlertType.ERROR, "Please enter an urgency");
+            a.showAndWait();
+            flag=false;
+        }
+        if (this.choiceGreenSpace.getSelectionModel().getSelectedItem() == null) {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Please select a green space");
+            a.showAndWait();
+            flag=false;
+        }
+        if (flag==true)
+            submitData();
+    }
+
+
     private void requestTask(){
         List<Task> taskList=controller.getTaskList();
         int i=1;
@@ -84,14 +212,7 @@ public class AddToDoListUI implements Runnable {
                     continue;
                 }
             }
-            String durationStr = "P" + days + "DT" + hours + "H" ;
-            try {
-                duration = Duration.parse(durationStr);
-            } catch (Exception e) {
-                duration = null;
-                System.out.println("Invalid duration, please try again.");
-                continue;
-            }
+
         }
         urgency=null;
         while (urgency==null){
@@ -124,33 +245,18 @@ public class AddToDoListUI implements Runnable {
         }
     }
 
-
-    private void requestGreenSpace() {
-        List<GreenSpace> greenSpaces=controller.getGreenSpaceList();
-        List<String> greenSpacesName=new ArrayList<String>();
-        for (GreenSpace g:greenSpaces) {
-            System.out.println(g);
-            greenSpacesName.add(g.getName());
-        }
-        greenSpaceName=null;
-        while(greenSpaceName==null){
-            greenSpaceName = Utils.readLineFromConsole("Select the name of the Green Space: ").toLowerCase();
-            if (!greenSpacesName.contains(greenSpaceName)){
-                greenSpaceName=null;
-                System.out.println("The name selected is not in the green space list, retry");
-                continue;
-            }
-        }
-    }
-
     private void submitData() {
 
         try {
             boolean result = controller.addToDoList(name,description,duration,urgency,greenSpaceName);
             if (result == true) {
                 System.out.println("\nEntry successfully registered in to-do-list!");
+                Alert a=new Alert(Alert.AlertType.CONFIRMATION,"Entry successfully registered in to-do-list!");
+                a.showAndWait();
             } else {
                 System.out.println("\nEntry registration failed!");
+                Alert a=new Alert(Alert.AlertType.ERROR,"Entry registration failed!");
+                a.showAndWait();
             }
         } catch (IllegalArgumentException e) {
             System.out.println("\nError: " + e.getMessage());
